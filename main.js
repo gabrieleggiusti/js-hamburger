0114e6f8e09ca0e59bee047e7b098aec02e55291
gabrieleggiusti <giusti98@gmail.com>
2022-04-07 17:44:28 +0200

    Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const activeMenuH = document.querySelector('.header-right > a');
+console.log(activeMenuH);
+
+const menuHamburger = document.querySelector('.hamburger-menu');
+console.log(menuHamburger);
+
+const activeClose = document.querySelector('.close');
